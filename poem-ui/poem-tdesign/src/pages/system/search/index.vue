f94a4736b732b8dbe6e7a82b8036b9ca812da8a3
baidurena <qyhe@baai.ac.cn>
2023-10-06 19:57:55 +0800

    1

---
+++ poem-ui/poem-tdesign/src/pages/system/search/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+<script setup lang="ts">
+const props = withDefaults(defineProps<{
+    options:{
+        name:string,
+        value:string,
+        key:string,
+        type:string
+    }[]
+}>(),{
+    options:()=>[{
+        name:'',
+        value:'',
+        key:'',
+        type:'input'
+    }]
+})
+</script>
+<style scoped lang="less">
+
+</style>
