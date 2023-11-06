0fa6f9c804f844c41d0236995a0fad1506efbc9b
hans <837713748@qq.com>
2023-11-06 12:18:09 +0800

    1.修改打包文件名

--- poem-admin/poem-library/poem-core/src/main/java/cn/poem/solon/admin/core/record/PasswordRecord.java
+++ poem-admin/poem-library/poem-core/src/main/java/cn/poem/solon/admin/core/record/PasswordRecord.java
@@ -2,10 +2,6 @@ package cn.poem.solon.admin.core.record;
 
 
 public record PasswordRecord(String salt, String password) {
-    public PasswordRecord(String salt, String password) {
-        this.salt=salt;
-        this.password=password;
-    }
 
 
 }
