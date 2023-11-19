bed21da39337fd6a519a59d4c77bcb166596ac9d
baidurena <qyhe@baai.ac.cn>
2023-11-19 19:55:50 +0800

    search搜索组件radioSelect模式无法正确显示placeholder

--- poem-ui/poem-admin/src/components/search/components/inputContent.vue
+++ poem-ui/poem-admin/src/components/search/components/inputContent.vue
@@ -3,10 +3,10 @@
         <t-input v-if="props.type === 'input'" v-model="value" :placeholder="props.placeholder || '请输入'"></t-input>
         <t-textarea v-else-if="props.type === 'textarea'" v-model="value"
             :placeholder="props.placeholder || '请输入'"></t-textarea>
-        <t-select v-else-if="props.type === 'select'" v-model="value">
+        <t-select v-else-if="props.type === 'select'" v-model="value" :placeholder="props.placeholder || '请输入'">
             <t-option v-for="(value, key) in radioOptions" :key="key" :value="key" :label="value" />
         </t-select>
-        <t-select v-else-if="props.type === 'dict'" v-model="value">
+        <t-select v-else-if="props.type === 'dict'" v-model="value" :placeholder="props.placeholder || '请输入'">
             <t-option v-for="(item, index) in dictOptions" :key="item.dictValue" :value="item.dictValue"
                 :label="item.dictLabel" />
         </t-select>
