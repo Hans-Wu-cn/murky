7452f8497fed00a18dd43430f7ba658c5f63894a
hans <837713748@qq.com>
2024-01-24 11:00:43 +0800

    1

--- murky-admin/murky-admin-system/murky-admin-system-biz/src/main/java/cn/murky/admin/system/biz/mapper/SysPostMapper.java
+++
@@ -1,7 +0,0 @@
-package cn.murky.admin.system.biz.mapper;
-
-import cn.murky.admin.system.biz.domain.entity.SysPost;
-import com.mybatisflex.core.BaseMapper;
-
-public interface SysPostMapper extends BaseMapper<SysPost> {
-}
