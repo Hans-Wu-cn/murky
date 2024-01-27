0e7265ee872f7410c21a60b82932a0ae28b55544
hans <837713748@qq.com>
2024-01-27 20:20:44 +0700

    租户库

---
+++ murky-tenant/murky-tenant-library/murky-tenant-core/src/main/java/cn/murky/tenant/core/MurkyServiceImpl.java
@@ -0,0 +1,45 @@
+package cn.murky.tenant.core;
+
+
+import cn.murky.security.entity.SecurityUserInfo;
+import cn.murky.security.utils.SecurityUtils;
+import cn.murky.tenant.core.utils.DataScopeUtils;
+import com.mybatisflex.core.BaseMapper;
+import com.mybatisflex.core.paginate.Page;
+import com.mybatisflex.core.query.QueryWrapper;
+import com.mybatisflex.core.service.IService;
+import com.mybatisflex.solon.service.impl.ServiceImpl;
+import org.noear.solon.core.bean.InitializingBean;
+
+import java.util.List;
+
+public abstract class MurkyServiceImpl<M extends BaseMapper<T>, T> extends ServiceImpl<M,T> implements IService<T>, InitializingBean {
+
+    protected QueryWrapper dataScope(QueryWrapper query, SecurityUserInfo userInfo){
+
+        DataScopeUtils.dataScope(query,userInfo,null);
+        return query;
+    }
+
+    protected QueryWrapper dataScope(QueryWrapper query){
+        return DataScopeUtils.dataScope(query);
+    }
+
+    public List<T> list(QueryWrapper query,SecurityUserInfo userInfo) {
+        return getMapper().selectListByQuery(dataScope(query,userInfo));
+    }
+
+    public List<T> list(QueryWrapper query) {
+        return getMapper().selectListByQuery(dataScope(query));
+    }
+
+    public Page<T> page(Page<T> page, QueryWrapper query,SecurityUserInfo userInfo) {
+        QueryWrapper queryWrapper = dataScope(query, userInfo);
+        return super.page(page,queryWrapper);
+    }
+
+    public Page<T> page(Page<T> page, QueryWrapper query) {
+        QueryWrapper queryWrapper = dataScope(query, SecurityUtils.getUserInfo());
+        return super.page(page,queryWrapper);
+    }
+}
---
+++ murky-tenant/murky-tenant-library/murky-tenant-core/src/main/java/cn/murky/tenant/core/utils/DataScopeUtils.java
@@ -0,0 +1,105 @@
+package cn.murky.tenant.core.utils;
+
+import cn.murky.common.enums.DataScope;
+import cn.murky.security.entity.SecurityUserInfo;
+import cn.murky.security.utils.SecurityUtils;
+import com.mybatisflex.core.query.CPI;
+import com.mybatisflex.core.query.QueryTable;
+import com.mybatisflex.core.query.QueryWrapper;
+import lombok.extern.slf4j.Slf4j;
+import org.noear.solon.Utils;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static java.lang.StringTemplate.STR;
+
+/**
+ * 数据权限工具类
+ * 提供数据权限静态调用方法
+ *
+ * @author hans
+ */
+@Slf4j
+public class DataScopeUtils {
+    public static final String DEPT_ANCESTORS_NAME = "department";
+    public static final String DEPT_TABLE_NAME = "sys_dept";
+
+    /**
+     * 数据权限方法，将query绑定数据权限sql
+     *
+     * @param query    query
+     * @param userInfo 用户信息
+     */
+    public static void dataScope(QueryWrapper query, SecurityUserInfo userInfo, String deptIdFrom) {
+        if (userInfo.getAdmin()) {
+            return;
+        }
+        DataScope dataScope = userInfo.getDataScope();
+        if (Utils.isNotEmpty(deptIdFrom)) {
+            deptIdFrom = STR."""
+                     "\{deptIdFrom}".""";
+        } else {
+            deptIdFrom = "";
+        }
+        StringBuilder sql = new StringBuilder();
+        if (DataScope.ALL == dataScope) {
+            return;
+        }
+        String deptColum = "fk_dept_id";
+        // 获取表名
+        List<QueryTable> tableList = CPI.getQueryTables(query);
+        if (Utils.isNotEmpty(tableList)) {
+            Set<String> tableNameSet = tableList.stream().map(QueryTable::getName).collect(Collectors.toSet());
+            if (Utils.isNotEmpty(tableNameSet) && tableNameSet.contains(DEPT_TABLE_NAME)) {
+                deptColum = "id";
+            }
+        }
+        if (DataScope.CUSTOMIZE == dataScope) {
+            sql.append(STR."""
+                          (\{deptIdFrom}\{deptColum} in (select fk_dept_id from sys_role_dept where fk_role_id = (\{userInfo.getFkRoleId()})))""");
+        }
+        if (DataScope.DEPARTMENT_BELOW == dataScope) {
+            sql.append(STR."""
+                         (\{deptIdFrom}\{deptColum} in (select \{DEPT_ANCESTORS_NAME}.fk_dept_id from sys_dept_ancestors as \{DEPT_ANCESTORS_NAME} where \{DEPT_ANCESTORS_NAME}.ancestors = \{userInfo.getDeptId()}) or \{deptIdFrom}\{deptColum}=\{userInfo.getDeptId()})
+                          """);
+
+        }
+        if (DataScope.DEPARTMENT == dataScope) {
+            sql.append(STR."""
+                         (\{deptIdFrom}\{deptColum}=\{userInfo.getDeptId().toString()})
+                        """);
+        }
+        if (DataScope.ONESELF == dataScope) {
+            sql.append(STR."""
+                         (\{deptIdFrom}create_user = \{userInfo.getUserId().toString()})
+                        """);
+        }
+
+        if (sql.isEmpty()) {
+            return;
+        }
+        query.and(sql.toString());
+        log.debug("[DataScopeUtils]->dataScope sql:{}", query.toSQL());
+    }
+
+    public static void dataScope(QueryWrapper query, SecurityUserInfo userInfo) {
+        dataScope(query, userInfo, null);
+    }
+
+    public static QueryWrapper dataScope(QueryWrapper query) {
+        dataScope(query, SecurityUtils.getUserInfo());
+        return query;
+    }
+
+    public static QueryWrapper dataScope(QueryWrapper query, String deptIdFrom) {
+        dataScope(query, SecurityUtils.getUserInfo(), deptIdFrom);
+        return query;
+    }
+
+
+    private DataScopeUtils() {
+    }
+
+}
